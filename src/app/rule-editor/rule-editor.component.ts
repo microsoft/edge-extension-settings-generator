dbea2e4c2b1b9e86dbd11ddd9875869ce3edcc05
Sohail Rajdev <sohailrajdev97@gmail.com>
2021-03-04 22:43:05 +0530

    ui: add rule-editor input

--- src/app/rule-editor/rule-editor.component.ts
+++ src/app/rule-editor/rule-editor.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import Rule from 'src/core/Rule/Rule';
 
 @Component({
   selector: 'app-rule-editor',
   templateUrl: './rule-editor.component.html',
-  styleUrls: ['./rule-editor.component.css']
+  styleUrls: ['./rule-editor.component.css'],
 })
 export class RuleEditorComponent implements OnInit {
+  @Input() rule: Rule;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
   }
 
+  isGlobalRule(): boolean {
+    return this.rule.getScope() === '*';
+  }
+
+  isUpdateUrlRule(): boolean {
+    return this.rule.getScope().startsWith('update_url:');
+  }
+
+  isExtensionRule(): boolean {
+    return !this.isGlobalRule() && !this.isUpdateUrlRule();
+  }
 }
