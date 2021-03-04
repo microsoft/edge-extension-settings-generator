8ddb126616fe28f28e27be16e1e6c0bf619eafb2
Sohail Rajdev <sohailrajdev97@gmail.com>
2021-03-04 22:43:04 +0530

    core: add Rule abstract class

---
+++ src/core/Rule/Rule.ts
@@ -0,0 +1,28 @@
+import { KeyValuePair } from '../KeyValuePair/KeyValuePair';
+
+export default abstract class Rule {
+  protected keyValuePairs: KeyValuePair[];
+
+  abstract canAddKeyValuePair(keyValuePair: KeyValuePair): boolean;
+  abstract getScope(): string;
+
+  addKeyValuePair(keyValuePair: KeyValuePair) {
+    if (!this.canAddKeyValuePair(keyValuePair)) {
+      throw new Error(
+        `ValidatedRule: ${keyValuePair.getKey()} not supported in rule ${this.getScope()}`
+      );
+    }
+    this.keyValuePairs.push(keyValuePair);
+  }
+
+  removeKey(keyToRemove: string) {
+    this.keyValuePairs = this.keyValuePairs.filter(
+      (pair) => pair.getKey() != keyToRemove
+    );
+  }
+
+  replaceKeyValuePair(keyValuePair: KeyValuePair) {
+    this.removeKey(keyValuePair.getKey());
+    this.addKeyValuePair(keyValuePair);
+  }
+}
